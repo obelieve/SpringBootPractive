23ce57c918533556421d4e48195ec0d6b69ea6d4
obelieve <zhangxyg@qq.com>
2025-01-08 16:06:02 +0800

    <20> @GetMapping/@PostMapping/@PutMapping/@DeleteMapping注解声明REST api，@PathVariable添加路径参数。

--- src/main/java/com/example/demo/customer/CustomerController.java
+++ src/main/java/com/example/demo/customer/CustomerController.java
@@ -1,8 +1,7 @@
 package com.example.demo.customer;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +22,22 @@ public class CustomerController {
     @GetMapping
     List<Customer> getCustomers() {
         return customerService.getCustomers();
+    }
+
+    @PostMapping
+    void createNewCustomer(@RequestBody Customer customer){
+        System.out.println("POST REQUEST..");
+        System.out.println(customer);
+    }
+
+    @PutMapping
+    void updateCustomer(@RequestBody Customer customer){
+        System.out.println("UPDATE REQUEST..");
+        System.out.println(customer);
+    }
+
+    @DeleteMapping(path = "{customerId}")
+    void deleteCustomer(@PathVariable("customerId") Long id){
+        System.out.println("DELETE REQUEST FOR CUSTOMER WITH ID "+id);
     }
 }
