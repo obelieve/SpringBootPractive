2139fa81e2d8e3be7963162567c8e2c056fc3ed0
obelieve <zhangxyg@qq.com>
2025-01-09 09:37:42 +0800

    <2>@JsonProperty添加JsonProperty.Access.WRITE_ONLY属性json写入

--- src/main/java/com/example/demo/customer/Customer.java
+++ src/main/java/com/example/demo/customer/Customer.java
@@ -11,6 +11,7 @@ public class Customer {
 
     private final Long id;
     private final String name;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private final String password;
 
     public Customer(Long id, String name, String password) {
