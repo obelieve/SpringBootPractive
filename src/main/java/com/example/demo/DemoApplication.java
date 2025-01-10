21a07829d93d0030172d6a91c7e7b90c1235a6fb
obelieve <zhangxyg@qq.com>
2025-01-10 16:35:01 +0800

    <63>添加终端命令行说明./mvnm、java -jar

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ *
+ * ./mvnm clean install      ⚠️：JAVA_HOME指定要和配置指定版本一样
+ * java -jar demo-0.0.1-SNAPSHOT.jar --spring.profiles.active=staging --server.port=3000
+ */
 @SpringBootApplication
 @EnableFeignClients
 public class DemoApplication {
