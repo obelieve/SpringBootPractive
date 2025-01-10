fcc87528ab2629a0440ab5c758726871e7412f4c
obelieve <zhangxyg@qq.com>
2025-01-10 10:48:32 +0800

    <46>CommandLineRunner中，打印jsonplaceholder接口内容。（PS:如果提示Feign Connection reset，换JDK试下）

---
+++ src/main/java/com/example/demo/jsonplaceholder/JSONPlaceHolderConfiguration.java
@@ -0,0 +1,26 @@
+package com.example.demo.jsonplaceholder;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author zxy
+ * @Date 2025/1/10
+ */
+@Configuration
+public class JSONPlaceHolderConfiguration {
+
+    @Bean("jsonplaceholder")
+    CommandLineRunner runner(JSONPlaceHolderClient
+                                     jsonPlaceHolderClient){
+        return args -> {
+            ///sout
+            System.out.println("https://jsonplaceholder.typicode.com/posts");
+            System.out.println(jsonPlaceHolderClient.getPosts().size());
+            System.out.println();
+            System.out.println("https://jsonplaceholder.typicode.com/posts/1");
+            System.out.println(jsonPlaceHolderClient.getPost(1));
+        };
+    }
+}
