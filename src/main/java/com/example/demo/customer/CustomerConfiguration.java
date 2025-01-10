07d9bbad8604ad944a3dd2e45b5359c74da764a3
obelieve <zhangxyg@qq.com>
2025-01-10 15:22:02 +0800

    <56>通过@Value(${..})和Environment来获取application.properties中的值

--- src/main/java/com/example/demo/customer/CustomerConfiguration.java
+++ src/main/java/com/example/demo/customer/CustomerConfiguration.java
@@ -1,9 +1,11 @@
 package com.example.demo.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 /**
  * @Author zxy
@@ -15,10 +17,18 @@ public class CustomerConfiguration {
     @Value("${app.useFakeCustomerRepo:false}")
     private Boolean useFakeCustomerRepo;
 
+    @Value("${info.company.name}")
+    private String companyName;
+
+    @Autowired
+    private Environment environment;
+
     @Bean
     CommandLineRunner commandLineRunner(){
         return args -> {
             System.out.println("Command line runner hooray");
+            System.out.println(companyName);
+            System.out.println(environment.getProperty("info.app.version"));
         };
     }
 
