189e63e3c8bb523cb4bbb0adc74b13d77f44955e
obelieve <zhangxyg@qq.com>
2025-01-10 11:04:21 +0800

    <47>添加Logger

--- src/main/java/com/example/demo/customer/CustomerService.java
+++ src/main/java/com/example/demo/customer/CustomerService.java
@@ -1,10 +1,13 @@
 package com.example.demo.customer;
 
 import com.example.demo.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 
 /**
  * @Author zxy
@@ -12,6 +15,8 @@ import java.util.List;
  */
 @Service
 public class CustomerService {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
 
     private final CustomerRepository customerRepository;
 
@@ -22,11 +27,16 @@ public class CustomerService {
 
 
     public List<Customer> getCustomers() {
+        LOGGER.info("getCustomers was called");
         return customerRepository.findAll();
     }
 
     public Customer getCustomer(Long id){
         return customerRepository.findById(id)
-                .orElseThrow(()-> new NotFoundException("customer with id "+id+" not found"));
+                .orElseThrow(()->{
+                        NotFoundException notFoundException = new NotFoundException("customer with id "+id+" not found");
+                        LOGGER.error("error in getting customer {}",id,notFoundException);
+                        return notFoundException;
+                });
     }
 }
