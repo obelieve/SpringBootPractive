1adc1565b7c51fc4a856fb42370126bb14469c47
obelieve <zhangxyg@qq.com>
2025-01-11 22:58:34 +0800

    <70>jdk 15预览版以上，一些场景下，使用record关键字消除样板代码

--- src/main/java/com/example/demo/exception/ApiException.java
+++ src/main/java/com/example/demo/exception/ApiException.java
@@ -1,6 +1,5 @@
 package com.example.demo.exception;
 
-import lombok.Data;
 import org.springframework.http.HttpStatus;
 
 import java.time.ZonedDateTime;
@@ -9,11 +8,10 @@ import java.time.ZonedDateTime;
  * @Author zxy
  * @Date 2025/1/9
  */
-@Data
-public class ApiException {
-
-    private final String message;
-    private final Throwable throwable;
-    private final HttpStatus httpStatus;
-    private final ZonedDateTime zonedDateTime;
+//private final,getter,public constructor,equals,hashcode
+///and to string
+public record ApiException(String message,
+                           Throwable throwable,
+                           HttpStatus httpStatus,
+                           ZonedDateTime zonedDateTime) {
 }
